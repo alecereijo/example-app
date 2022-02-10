a456a26a8746f8a87d005c894d9674db5d9d4fd9
alecereijo <alecereijo@gmail.com>
2022-02-10 14:37:25 +0100

    cambios

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+    <?= $post; ?>
+    </article>
+    <a href="/">Volver</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+    <h1><a href="/post">My first post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt fugiat impedit alias quia excepturi. Hic nihil nobis vero. Possimus quibusdam aliquam aut quidem cupiditate non doloribus quo harum accusantium magni!
+    </p>
+    </article>
+
+    <article>
+    <h1><a href="/post">My second post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt fugiat impedit alias quia excepturi. Hic nihil nobis vero. Possimus quibusdam aliquam aut quidem cupiditate non doloribus quo harum accusantium magni!
+    </p>
+    </article>
+
+    <article>
+    <h1><a href="/post">My third post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt fugiat impedit alias quia excepturi. Hic nihil nobis vero. Possimus quibusdam aliquam aut quidem cupiditate non doloribus quo harum accusantium magni!
+    </p>
+    </article>
+</body>
